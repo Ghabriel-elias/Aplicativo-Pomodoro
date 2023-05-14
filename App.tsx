086bc32a591c5d5e29c166530e4d6c37e4b0f6f7
ghabriel <ghsouza1013@gmail.com>
2023-05-14 15:55:19 -0400

    hotfix

--- App.tsx
+++ App.tsx
@@ -43,9 +43,7 @@ export default function App() {
      const config = await AsyncStorage.getItem("config")
      if(config === 'standart'){
       setRoute('Standard')
-     } else {
-      setRoute('Standard')
-     }
+     } 
     }
     Load()
   }, [])
